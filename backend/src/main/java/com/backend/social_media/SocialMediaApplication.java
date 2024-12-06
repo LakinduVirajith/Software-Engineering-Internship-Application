3ff3d10d2416a242543b59ff82da6f6a8038a42c
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-12-06 16:05:17 +0530

    the backend application initiated

---
+++ backend/src/main/java/com/backend/social_media/SocialMediaApplication.java
@@ -0,0 +1,24 @@
+package com.backend.social_media;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SocialMediaApplication {
+
+	@Value("${server.port}")
+	private String port;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaApplication.class, args);
+	}
+
+	@GetMapping
+	public String home() {
+		return "Social Media App is up and running on port " + port;
+	}
+}
